9d2209e90d02255fa2720a467f4a2aca0831894d
isard <isard@debian>
2026-01-20 08:05:36 +0100

    Fix IOC: tests + ensure Django 5.1 detection

--- polls/tests.py
+++ polls/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 from .models import Question
 
-class BasicTest(TestCase):
+class IOCBasicTest(TestCase):
     def test_create_question(self):
-        q = Question.objects.create(question_text="Test", pub_date=timezone.now())
+        Question.objects.create(question_text="Test", pub_date=timezone.now())
         self.assertEqual(Question.objects.count(), 1)
