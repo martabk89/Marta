62724bffe537039442bc15573215d2cb1f2d4260
isard <isard@debian>
2026-01-20 08:18:33 +0100

    Fix tests: remove root tests.py causing error

--- tests.py
+++ tests.py
@@ -1,57 +1,29 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
 from django.test import TestCase
 from django.utils import timezone
-from .models import Question
-import datetime
-from django.test import TestCase
-from django.utils import timezone
-from .models import Question
+from .models import Question, Choice
 
-class BasicTest(TestCase):
+
+class IOCBasicTest(TestCase):
+
     def test_create_question(self):
-        q = Question.objects.create(question_text="Test", pub_date=timezone.now())
+        """Comprobar que se puede crear una Question"""
+        q = Question.objects.create(
+            question_text="¿Test IOC?",
+            pub_date=timezone.now()
+        )
         self.assertEqual(Question.objects.count(), 1)
+        self.assertEqual(str(q), "¿Test IOC?")
 
-
-class QuestionModelTests(TestCase):
-    def test_question_str(self):
-        q = Question(question_text="Hola", pub_date=timezone.now())
-        self.assertEqual(str(q), "Hola")
- 
-class MySeleniumTests(StaticLiveServerTestCase):
-    # carregar una BD de test
-    fixtures = ['testdb.json',]
-
-@classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        opts = Options()
-        cls.selenium = WebDriver(options=opts)
-        cls.selenium.implicitly_wait(5)
-
-@classmethod
-    def tearDownClass(cls):
-        # tanquem browser
-        # comentar la propera línia si volem veure el resultat de l'execució al navegador
-        cls.selenium.quit()
-        super().tearDownClass()
- 
-    def test_login(self):
-        # anem directament a la pàgina d'accés a l'admin panel
-        self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
-
-# comprovem que el títol de la pàgina és el que esperem
-        self.assertEqual( self.selenium.title , "Log in | Django site admin" )
- 
-        # introduïm dades de login i cliquem el botó "Log in" per entrar
-        username_input = self.selenium.find_element(By.NAME,"username")
-        username_input.send_keys('admin')
-        password_input = self.selenium.find_element(By.NAME,"password")
-        password_input.send_keys('admin123')
-        self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
- 
-        # testejem que hem entrat a l'admin panel comprovant el títol de la pàgina
-        self.assertEqual( self.selenium.title , "Site administration | Django site admin" 
+    def test_create_choice(self):
+        """Comprobar que se puede crear una Choice asociada"""
+        q = Question.objects.create(
+            question_text="¿Test Choice?",
+            pub_date=timezone.now()
+        )
+        c = Choice.objects.create(
+            question=q,
+            choice_text="Sí",
+            votes=0
+        )
+        self.assertEqual(Choice.objects.count(), 1)
+        self.assertEqual(c.question, q)
