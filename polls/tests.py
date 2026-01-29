2aa2ef92aba8b0c9e6d31ec37d02e3a954743de2
isard <isard@debian>
2026-01-29 09:14:53 +0100

    Test Selenium: staff sin permisos no puede crear Users ni Questions

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,54 @@
-from django.test import TestCase
-from django.utils import timezone
-from .models import Question
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-class IOCBasicTest(TestCase):
-    def test_create_question(self):
-        Question.objects.create(question_text="Test IOC", pub_date=timezone.now())
-        self.assertEqual(Question.objects.count(), 1)
+class StaffWithoutPermissionsTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        opts = Options()
+        opts.add_argument("--headless")
+        cls.selenium = webdriver.Firefox(options=opts)
+        cls.selenium.implicitly_wait(5)
+
+        # superusuario
+        admin = User.objects.create_superuser(
+            "isard", "isard@isard.com", "pirineus"
+        )
+
+        # usuario staff SIN permisos
+        staff = User.objects.create_user(
+            "staff1", "staff@staff.com", "staffpass"
+        )
+        staff.is_staff = True
+        staff.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_staff_cannot_create_users_or_questions(self):
+        # login como staff
+        self.selenium.get(f"{self.live_server_url}/admin/login/")
+        self.selenium.find_element(By.NAME, "username").send_keys("staff1")
+        self.selenium.find_element(By.NAME, "password").send_keys("staffpass")
+        self.selenium.find_element(By.XPATH, "//input[@value='Log in']").click()
+
+        # comprobar que NO existe Users
+        try:
+            self.selenium.find_element(By.LINK_TEXT, "Users")
+            assert False, "El usuario staff NO debería ver Users"
+        except NoSuchElementException:
+            pass
+
+        # comprobar que NO existe Questions
+        try:
+            self.selenium.find_element(By.LINK_TEXT, "Questions")
+            assert False, "El usuario staff NO debería ver Questions"
+        except NoSuchElementException:
+            pass
